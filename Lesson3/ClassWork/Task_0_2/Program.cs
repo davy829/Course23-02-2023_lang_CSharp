7eb6c782752823f5ac083054be88e7858cb73fea
Andrey DALI <daandrey485@gmail.com>
2023-03-03 17:10:29 +0300

    Leeson3 ClassWork Task_0_2 redy

--- Lesson3/ClassWork/Task_0_2/Program.cs
+++ Lesson3/ClassWork/Task_0_2/Program.cs
@@ -2,7 +2,7 @@
 
 double Distans(double x1, double y1, double x2, double y2)
 {
-    return Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y1 - y2, 2)));
+    return Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y1 - y2, 2))); //квадратный корень
 }
 
 Console.WriteLine(Distans(1,1,2,2));
