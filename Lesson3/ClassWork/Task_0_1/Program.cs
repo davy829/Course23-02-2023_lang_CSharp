11e92ce38c2e091d246be002fd47a9a240fdb5db
Andrey DALI <daandrey485@gmail.com>
2023-03-03 17:09:37 +0300

    Leeson3 ClassWork Task_0_1 redy

--- Lesson3/ClassWork/Task_0_1/Program.cs
+++ Lesson3/ClassWork/Task_0_1/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// по номеру четверти графика определяет координаты возможные
 Console.Write("Введите номер четверти __");
 int MyQuater = int.Parse(Console.ReadLine()!);
 
