d7d664dda1f37c3c73e2176ba89c641e824ed4cc
Andrey DALI <daandrey485@gmail.com>
2023-03-03 16:52:41 +0300

    Leeson1 HomeWork Task_0_0 redy

--- Lesson1/HomeWork/HW_Task_1/Program.cs
+++ Lesson1/HomeWork/HW_Task_1/Program.cs
@@ -1,12 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// показывает какое из введенных чисел больше
 Console.Write("Введите два разных числа _");
 int MyNum = int.Parse(Console.ReadLine()!);
 int MyNum2 = int.Parse(Console.ReadLine()!);
 if (MyNum > MyNum2)
 {
-    Console.WriteLine($" Больше :{MyNum}");
+    Console.WriteLine($" {MyNum} > {MyNum}");
 }
 else
 {
-    Console.WriteLine($" Больше :{MyNum2}");  
+    Console.WriteLine($" {MyNum} < {MyNum2}");  
 }
