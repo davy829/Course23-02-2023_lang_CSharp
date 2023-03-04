83b215e1247c8c4659472fbabd1bfcd1f2ce5821
Andrey DALI <daandrey485@gmail.com>
2023-03-04 10:41:16 +0300

    Leeson3 HomeWork Task_3 remake-> round(val,2)

--- Lesson3/HomeWork/HW_Task_2/Program.cs
+++ Lesson3/HomeWork/HW_Task_2/Program.cs
@@ -1,10 +1,10 @@
-﻿// расстояние между двумя точками в 3д
+﻿// расстояние между двумя точками в 3д 56
 Console.WriteLine("Введите значения 6 шт/ через запятую _");
 
 void Dis3d(double x1, double y1, double z1, double x2, double y2, double z2)
 {
    double res = Math.Sqrt(Math.Pow(x1 - x2, 2)+ Math.Pow(y1 - y2, 2)+Math.Pow(z1 - z2, 2));
-   Console.WriteLine($"Расстояние между двумя точками = {Math.Round(res)} ");
+   Console.WriteLine($"Расстояние между двумя точками = {Math.Round(res,2)} ");
 }
 Dis3d(1,7,9,3,5,8);
 
