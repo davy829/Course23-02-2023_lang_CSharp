02587998863257e25b5f4eecf03910dc1f50ab47
Andrey DALI <daandrey485@gmail.com>
2023-03-07 23:46:40 +0300

    Lesson_4 ClassWork task_2 redy

---
+++ Lesson4/ClassWork/Task_0_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// выводит произведение от одного до введенного числа 
+Console.Write("Введите число ___");
+int mynum = int.Parse(Console.ReadLine()!);
+int Proizvedenie(int num)
+{
+    int res = 1; // на 0 множить бесполезно поэтому с 1
+    for (int i = 0; i < num; i++) //цикл до введенного
+    {
+        res = res * (i + 1); //считаем произведение
+        Console.WriteLine($"{i} x {i+1} = {res} "); //выводим пример
+    }
+    return res; // вернем результат
+}
+Console.WriteLine(Proizvedenie(mynum)); // выведем результат
