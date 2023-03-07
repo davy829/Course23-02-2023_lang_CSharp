80e73a504aa1f034935f85dfaa95fe6a013ea2f0
Andrey DALI <daandrey485@gmail.com>
2023-03-08 00:13:36 +0300

    Lesson_4 ClassWork task_2 redy масив с нулями и еденицами

---
+++ Lesson4/ClassWork/Task_0_3/Program.cs
@@ -0,0 +1,14 @@
+﻿int length = 10;   // длина массива 
+int[] Myarray = new int[length]; // создаем массив
+
+void FillArray(int[] array, int len)
+{
+    for (int i = 0; i < len; i++)
+  {
+   int OneZero = new Random().Next(0,2); // произвольное 0 или 1
+   array[i] = OneZero; // наполняем
+   Console.Write($"{array[i]}, "); // выводим 
+  }
+  
+}
+FillArray(Myarray,length); // зовем функцию даем ей массив и длинну
