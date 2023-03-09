e7b4dc9794c83b0c11b3f0f76da509873322aea1
Andrey DALI <daandrey485@gmail.com>
2023-03-09 20:34:21 +0300

    Lesson4 homework Task_3 modify

--- Lesson4/HomeWork/HW_Task_3/Program.cs
+++ Lesson4/HomeWork/HW_Task_3/Program.cs
@@ -2,21 +2,29 @@
 //8 элементов случайными числами и выводит их на экран.
 //Оформите заполнение массива и вывод в виде функции
 //(пригодится в следующих задачах)
-
-Console.Write("Введите размер массива___"); 
-int len = int.Parse(Console.ReadLine()!); // принимаем размер массива
-int[] massiv = new int[len]; // создаем массив 
-
-
-void FullArray(int[] MyArray)
+int[] FullArray(int Size, int Start1, int End1)
 {
-    int len = MyArray.Length; // узнаем длину 
-    for (int i = 0; i < len; i++)  // цикл равен длине
-    {
-        MyArray[i] = new Random().Next(1,99); //наполняем случайными
-        Console.Write($",{MyArray[i]}"); //выводим на экран
-    }
-   
+    int[] Arr = new int[Size]; // создаем массив 
+    for (int i = 0; i < Arr.Length; i++)  // цикл равен длине
+        Arr[i] = new Random().Next(Start1, End1); //наполняем случайными
+    return Arr;
 }
 
-FullArray(massiv);
+void PrintArray(int[] Array)
+{
+    int len = Array.Length;
+    for (int i = 0; i < len; i++)
+        Console.Write($"{Array[i]} "); //выводим на экран
+    Console.WriteLine();
+}
+
+
+Console.Write("Введите размер массива___");
+int len = int.Parse(Console.ReadLine()!); // принимаем размер массива
+Console.Write("Введите стартовое __");
+int Start2 = int.Parse(Console.ReadLine()!); // принимаем меньший значение
+Console.Write("Введите финишное __");
+int End2 = int.Parse(Console.ReadLine()!); // принимаем болшее значение
+
+int[] massiv = FullArray(len, Start2, End2);
+PrintArray(massiv);
