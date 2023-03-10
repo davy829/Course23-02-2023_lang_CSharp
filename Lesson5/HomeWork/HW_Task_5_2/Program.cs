417d4b5a82a725d51b37b88663d07c5a0b6ecd18
Andrey DALI <daandrey485@gmail.com>
2023-03-10 21:55:32 +0300

    Lesson5 homeWork Task_5_2 find in array negativ summ

---
+++ Lesson5/HomeWork/HW_Task_5_2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный
+//случайными числами. Найдите сумму элементов, стоящих
+//на нечётных позициях.
+
+Console.Write("Введите размер массива_");
+int Size = int.Parse(Console.ReadLine()!);
+Console.Write("Введите меншее значение в массиве_");
+int from = int.Parse(Console.ReadLine()!);
+Console.Write("Введите большее значение в массиве_");
+int to = int.Parse(Console.ReadLine()!);
+
+int[] FullArray(int Len, int Start, int End)
+{
+    int[] arr = new int[Len];
+    for (int i = 0; i < Len; i++)
+    {
+        arr[i] = new Random().Next(Start, End);
+    }
+    return arr;
+}
+
+void PrintArr(int[] Array)
+{
+    int len = Array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"{Array[i]} ");
+    }
+}
+
+int FindInArray(int[] Array)
+{
+    int len = Array.Length;
+    int summ = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (i % 2 == 1)
+        {
+            summ = summ + Array[i];
+         Console.Write($"{Array[i]} + ");   
+        }
+    }
+    Console.Write($"={summ}");
+    return summ;
+}
+
+int[] massiv = FullArray(Size, from, to);
+PrintArr(massiv);
+Console.WriteLine();
+FindInArray(massiv);
