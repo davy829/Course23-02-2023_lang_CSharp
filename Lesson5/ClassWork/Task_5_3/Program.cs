e75c8fdd07eb6f42f4224b0f9399de5fe324d15b
Andrey DALI <daandrey485@gmail.com>
2023-03-10 10:22:57 +0300

    Lesson5 ClassWork Task_5_3 find in array диапозон мин и макс

---
+++ Lesson5/ClassWork/Task_5_3/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 3: Задайте одномерный массив из 123 случайных чисел.
+//Найдите количество элементов массива, значения которых лежат в
+//отрезке [10,99]. 
+
+Console.Write("Введите размер массива_");
+int Size = int.Parse(Console.ReadLine()!);
+Console.Write("Введите меншее значение в массиве_");
+int from = int.Parse(Console.ReadLine()!);
+Console.Write("Введите большее значение в массиве_");
+int to = int.Parse(Console.ReadLine()!);
+
+int[] FullArray(int ArrayLen, int ArrayMin,int ArrayMax)
+{
+    int[] arr = new int[ArrayLen];
+    for (int i = 0; i < ArrayLen; i++)
+    {
+        arr[i] = new Random().Next(ArrayMin,ArrayMax);
+    }
+    return arr;
+}
+
+void PrinrMyArr(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void FindNum(int[] FArray,int FindMax,int FindMin)
+{
+    int count =0;
+  for (int i = 0; i < FArray.Length; i++)
+  {
+    if (FArray[i] > 10 && FArray[i] < 99)
+    count ++;
+  }
+  Console.Write($"в этом массиве элементов > 10 < 99 всего: {count}");
+}
+
+
+
+int[] massiv = FullArray(Size,from,to);
+PrinrMyArr(massiv);
+Console.WriteLine();
+Console.Write("Что ищем? Введите мин_");
+int ArrMin1 = int.Parse(Console.ReadLine()!);
+Console.Write("Что ищем? Введите мax_");
+int ArrMax1 = int.Parse(Console.ReadLine()!);
+FindNum(massiv,ArrMin1,ArrMax1);
