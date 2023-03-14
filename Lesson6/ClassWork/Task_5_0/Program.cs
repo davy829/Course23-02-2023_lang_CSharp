32f96d0c7b168bf594c516ab6936f538e7a7947a
Andrey DALI <daandrey485@gmail.com>
2023-03-14 23:29:10 +0300

    Lesson6 ClassWork Task_3,2,3 redy

--- Lesson6/ClassWork/Task_5_0/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите размер массива_");
-int Size = int.Parse(Console.ReadLine()!);
-Console.Write("Введите меншее значение в массиве_");
-int from = int.Parse(Console.ReadLine()!);
-Console.Write("Введите большее значение в массиве_");
-int to = int.Parse(Console.ReadLine()!);
---
+++ Lesson6/ClassWork/Task_6_2/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите длинну массива___");
+int Size = int.Parse(Console.ReadLine()!);
+Console.Write("Введите минимальное значение массива___");
+int min = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное массива___");
+int max = int.Parse(Console.ReadLine()!);
+
+
+int[] FullArray(int len, int from, int to)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+void PrintArr(int[] array)
+{
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+void ReturnArr(int[] array)
+{
+    int len = array.Length;
+    for (int i = 0; i < len / 2; i++)
+    {
+        (array[i], array[len - i-1]) = (array[len - i-1], array[i]);
+    }
+   
+}
+
+
+int[] massiv = FullArray(Size, min, max);
+PrintArr(massiv);
+Console.WriteLine();
+ReturnArr(massiv);
+PrintArr(massiv);
