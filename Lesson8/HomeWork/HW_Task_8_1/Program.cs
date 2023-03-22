a4fc45b893acff55202e42dd61e651a5ce93cd4b
Andrey DALI <daandrey485@gmail.com>
2023-03-22 20:59:19 +0300

    Lesson8 HomeWork Task_8_2 find row with min summ elemets

---
+++ Lesson8/HomeWork/HW_Task_8_1/Program.cs
@@ -0,0 +1,71 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая
+//упорядочит по убыванию элементы каждой строки двумерного массива.
+
+
+int[,] CreateArray(int rowCount,int colCount)
+{
+    int[,] myArray = new int[rowCount,colCount];
+    return myArray;
+}
+
+void FullArray(int[,] Array)
+{
+    int row = Array.GetLength(0);
+    int col = Array.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+           Array[i,j] = new Random().Next(10,55); 
+        }
+    }
+}
+
+string PrintArray(int[,] Array)
+{
+    string resault = string.Empty;
+    int row = Array.GetLength(0);
+    int col = Array.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+       for (int j = 0; j < col; j++)
+       {
+         resault += Array[i,j];
+       if (j != col-1)
+         resault += "\t";
+         else
+         resault += "\n";
+       } 
+    }
+    return resault;
+}
+
+
+void SortDownRows(int[,] Array)
+{
+   int min = Array[0,0];
+    int row = Array.GetLength(0);
+    int col = Array.GetLength(1);
+    for (int i = 1; i < col; i++)
+    {
+       for (int j = 1; j < row; j++)
+       {
+        if (min < Array[j,i])
+           (min , Array[j,i]) = (Array[j,i], min);
+
+       } 
+    }
+
+}
+
+
+
+int[,] massiv = CreateArray(3,3);
+FullArray(massiv);
+string massivToString = PrintArray(massiv);
+Console.WriteLine(massivToString);
+SortDownRows(massiv);
+string myArraySort = PrintArray(massiv);
+Console.WriteLine(myArraySort);
+
+//  TODO
---
+++ Lesson8/HomeWork/HW_Task_8_2/Program.cs
@@ -0,0 +1,90 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите
+//программу, которая будет находить строку с наименьшей суммой элементов.
+
+void FullArray(int[,] Array)
+{
+    int row = Array.GetLength(0);
+    int col = Array.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            Array[i, j] = new Random().Next(10, 55);
+        }
+    }
+}
+
+void PrintArray2d(int[,] array)
+{
+    // string res = string.Empty;
+    int row = array.GetLength(0);
+    int col = array.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            /*        res += array[i,j];
+                   if (j != col-1)
+                   res += "\t";
+                   else
+                   res += "\n"; */
+            Console.Write($"{array[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+    //return res;
+}
+
+int[] SummRowsElement(int[,] myArray)
+{
+
+    int row = myArray.GetLength(0);
+    int col = myArray.GetLength(1);
+    int[] mass = new int[row];
+    for (int i = 0; i < row; i++)
+    {
+        int summ = 0;
+        for (int j = 0; j < col; j++)
+        {
+            summ += myArray[i, j];
+
+        }
+        mass[i] = summ;
+        //  Console.WriteLine($"{i} - > {summ}");
+    }
+    return mass;
+}
+
+int FindMinSummRow(int[] myArr)
+{
+    int min = myArr[0];
+    int minIndex = 0;
+    for (int i = 1; i < myArr.Length; i++)
+    {
+        if (myArr[i] < min)
+        {
+            min = myArr[i];
+            minIndex = i + 1;
+        }
+    }
+    return minIndex;
+}
+
+string PrintRowMinSumm(int[,] myArray2d, int myRow)
+{
+    string rowMinSumValue = string.Empty;
+    for (int i = 0; i < myArray2d.GetLength(1); i++)
+    {
+        rowMinSumValue += $" {myArray2d[myRow, i]}";
+    }
+    return rowMinSumValue;
+}
+
+int[,] massiv = new int[8, 3];
+FullArray(massiv);
+PrintArray2d(massiv);
+int[] myArray = SummRowsElement(massiv);
+int res = FindMinSummRow(myArray);
+Console.WriteLine($"номер строки {res} с минимальной суммой elements");
+string MinRowSumm = PrintRowMinSumm(massiv, res);
+Console.WriteLine($"строкa {MinRowSumm} с минимальной суммой elements");
