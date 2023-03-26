39a8f233073bb61ce1de1f447c64e307dfa991a8
Andrey DALI <daandrey485@gmail.com>
2023-03-27 00:13:19 +0300

    Lesson9 HomeWork Task_9_0 Even natural nums with recursia

---
+++ Lesson9/HomeWork/HW_Task_9_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все
+//чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+int UserInput(string userData)
+{
+    Console.Write(userData);
+    return Convert.ToInt32(Console.ReadLine()!);
+}
+int numM = UserInput("Input your M number_");
+int numN = UserInput("Input your N number_");
+
+void EvenNumbers(int M, int N)
+{
+    if (M < N)
+    {
+
+        if (M == N) return;
+        EvenNumbers(M, N - 1);
+        if (N % 2 == 0) Console.Write($"{N}\t ");
+    }
+    else
+    {
+        if (M == N) return;
+        EvenNumbers(M - 1, N);
+        if (M % 2 == 0) Console.Write($"{M}\t ");
+    }
+}
+
+EvenNumbers(numM, numN);
