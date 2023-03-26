175152f29ef992a9abef85fdbdb3a81ce9a1e6b3
Andrey DALI <daandrey485@gmail.com>
2023-03-26 23:51:20 +0300

    Lesson9 ClassWork Task_9_0 recusia  Degree A to B

--- Lesson9/ClassWork/Task_9_3/Program.cs
+++ Lesson9/ClassWork/Task_9_3/Program.cs
@@ -1,4 +1,21 @@
 ﻿// Задача 4: Напишите программу, которая на вход принимает два числа A и B, и
 //возводит число А в целую степень B с помощью рекурсии.
 
-Console.WriteLine("Hello, World!");
+int UserInput(string userData)
+{
+    Console.Write(userData);
+    return Convert.ToInt32(Console.ReadLine()!);
+}
+
+int numA = UserInput("Input your A number_");
+int numB = UserInput("Input your B number_");
+
+
+int Degree(int a, int b)
+{
+    if (b == 0) return 1;
+   return a * Degree(a ,b-1);
+}
+
+int res = Degree(numA,numB);
+Console.Write($"{res }");
