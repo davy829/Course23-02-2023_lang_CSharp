1281d51a89665deb6a842f67480154a01a51b248
Andrey DALI <daandrey485@gmail.com>
2023-03-26 18:14:32 +0300

    Lesson8 HomeWork Task_8_4 удалить из массива строку и столбец с мин знач

--- Lesson8/HomeWork/HW_Task_8_4/Program.cs
+++ Lesson8/HomeWork/HW_Task_8_4/Program.cs
@@ -5,67 +5,95 @@
 
 void FullArray(int[,] myArray)
 {
-    int row=myArray.GetLength(0);
-    int col=myArray.GetLength(1);
-   for (int i = 0; i < row; i++)
-   {
-    for (int j = 0; j < col; j++)
-    {
-        myArray[i,j]= new Random().Next(10,50);
-    }
-   }
-}
-
-string PrintArray(int[,]myArray)
-{
-    string ArrToStr = string.Empty;
-       int row=myArray.GetLength(0);
-    int col=myArray.GetLength(1);
-   for (int i = 0; i < row; i++)
-   {
-    for (int j = 0; j < col; j++)
-    {
-        ArrToStr+=myArray[i,j];
-      if (j != col-1)
-       ArrToStr += "\t";
-       else
-       ArrToStr+= "\n";
-
-    }
-   }
-   return ArrToStr;
-}
-
-void MinElementValue(int[,] myArray)
-{
-    int min=myArray[0,0];
-    int minI=0;
-    int minJ=0;
     int row = myArray.GetLength(0);
     int col = myArray.GetLength(1);
-    for (int i = 1; i < row; i++)
+    for (int i = 0; i < row; i++)
     {
-        for (int j = 1; j < col; j++)
+        for (int j = 0; j < col; j++)
         {
-          if (myArray[i,j] < min)
-          {
-            min = myArray[i,j];
-            minI=i;
-            minJ=j;
-          }
-           
+            myArray[i, j] = new Random().Next(10, 50);
         }
-
-          
     }
-Console.WriteLine($" i {minI} - > j {minJ}");
 }
 
+string PrintArray(int[,] myArray)
+{
+    string ArrToStr = string.Empty;
+    int row = myArray.GetLength(0);
+    int col = myArray.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            ArrToStr += myArray[i, j];
+            if (j != col - 1)
+                ArrToStr += "\t";
+            else
+                ArrToStr += "\n";
+
+        }
+    }
+    return ArrToStr;
+}
+
+int[] MinElementValue(int[,] myArray)
+{
+
+    int min = myArray[0, 0];
+    int minI = 0;
+    int minJ = 0;
+    int row = myArray.GetLength(0);
+    int col = myArray.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (myArray[i, j] < min)
+            {
+                min = myArray[i, j];
+                minI = i;
+                minJ = j;
+            }
+
+        }
 
 
-int[,] Array = new int[4,4];
+    }
+    Console.WriteLine($" строка {minI+1} - > столбец {minJ+1} значение {min}");
+    return new int[] { min, minJ };
+}
+
+int[,] CreateArrayWithoutMinRow(int[,] myArray, int[] ArrayIndex)
+{
+    int rowDel = ArrayIndex[0];
+    int colDel = ArrayIndex[1];
+    int row = myArray.GetLength(0);
+    int col = myArray.GetLength(1);
+    int[,] resArray = new int[row - 1, col - 1];
+
+    for (int i = 0, k = 0; i < row-1; k++, i++)
+    {
+        if (i != rowDel)
+            for (int j = 0, l = 0; j < col; l++, j++)
+            {
+                if (j != colDel)
+                    resArray[k, l] = myArray[i, j];
+                else l--;
+            }
+        else
+            k--;
+    }
+    return resArray;
+}
+
+int[,] Array = new int[3, 4];
 FullArray(Array);
 string res = PrintArray(Array);
 Console.Write(res);
-MinElementValue(Array);
 
+
+MinElementValue(Array);
+int[] DelRowCol = MinElementValue(Array);
+int[,] resaultDelete = CreateArrayWithoutMinRow(Array, DelRowCol);
+string resDel = PrintArray(resaultDelete);
+Console.Write(resDel);
